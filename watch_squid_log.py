f0135ae846457fca9ea39d1bade044464ad33d52
Luis Emiro Linares Garcia <luis.linares@cern.ch>
2013-09-11 16:05:04 +0200

    Refined parsing of URL and Frontier-id fields.

--- watch_squid_log.py
+++ watch_squid_log.py
@@ -23,30 +23,53 @@
     <st     Reply size including HTTP headers
 """
 
-import time 
-import re
 import fileinput
+import re
+import sys
+import time 
 
-#squid_access_re = r"""^(?P<client_ip>\d+(?:\.\d+){3}) (?P<user_ident>\S+) (?P<user_name>\S+) \[(?P<timestamp>\S+ \S+)\] "(?P<method>\S+) (?P<url>\S+) HTTP/(?P<proto_version>\S+)" (?P<code>\d+) (?P<size>\d+) (?P<req_status>[^: ]+):(?P<hierarchy_status>\S+) (?P<resp_time>\d+) "(?P<frontier_id>[^"]+)" "(?P<IMS>[^"]*)"$"""
-squid_access_re = r"""^(?P<client_ip>\S+) (?P<user_ident>\S+) (?P<user_name>\S+) \[(?:\S+ \S+)\] "(?P<method>\S+) (?P<url>\S+) HTTP/(?P<proto_version>\S+)" (?P<code>\d+) (?P<size>\d+) (?P<req_status>[^: ]+):(?P<hierarchy_status>\S+) (?P<resp_time>\d+) "(?P<frontier_id>[^"]+)" "(?P<IMS>[^"]*)"$"""
+squid_access_re = r"""^(?P<client_ip>\S+) (?P<user_ident>\S+) (?P<user_name>\S+) \[(?:\S+ \S+)\] "(?P<method>\S+) (?:[^/]+)[/]+(?P<server>[^/]+)/(?P<servlet>[^/]+)/(?P<query_name>[^/]+)[/?](?P<query>\S+) HTTP/(?P<proto_version>\S+)" (?P<code>\d+) (?P<size>\d+) (?P<req_status>[^: ]+):(?P<hierarchy_status>\S+) (?P<resp_time>\d+) "(?P<frontier_id>[^"]+)" "(?P<IMS>[^"]*)"$"""
 squid_regex = re.compile(squid_access_re)
 
-for line in fileinput.input():
+def main():
 
-    record = squid_regex.match(line).groupdict()
+    try:
+        for line in fileinput.input():
 
-    """ Parsing the timestamp is unnecessary and wasteful :)
-    ts, tz_offset = record['timestamp'].split()
-    epoch = int (time.mktime (time.strptime (ts, "%d/%b/%Y:%H:%M:%S")))
-    offset = 3600*int(tz_offset[1:3]) + 60*int(tz_offset[3:])
-    if tz_offset[0] == '-': epoch -= offset
-    else: epoch += offset
-    record['timestamp'] = epoch
-    """
-    record['timestamp'] = time.time()
+            record = parse_log_line (line)
+            if record is None: return 1
+            
+            if 'fid_userdn' in record:
+                print '%.6f' % record['timestamp'], '(%s)' % record['fid_userdn'], record['client_ip'], record['fid_sw_release'], record['size'], record['fid_uid'], '>', record['server'], record['query'], record['servlet']
 
-    #print record['timestamp'], record['client_ip'], record['frontier_id'], record['size']
-    print '%.6f' % record['timestamp'],  record['client_ip'], record['frontier_id'], record['size']
+    except KeyboardInterrupt:
+        return 0
 
-#class 
 
+def parse_log_line (line):
+
+    try:
+        record = squid_regex.match(line).groupdict()
+
+        record['timestamp'] = time.time()
+
+        frontier_id_parts = record['frontier_id'].split()
+        record.pop('frontier_id')
+
+        record['fid_sw_release'] = frontier_id_parts[0]
+        record['fid_sw_version'] = frontier_id_parts[1]
+
+        if len(frontier_id_parts) > 2:
+            record['fid_pid'] = frontier_id_parts[2]
+        if len(frontier_id_parts) > 3:
+            record['fid_uid'] = frontier_id_parts[3]
+            record['fid_userdn'] = ' '.join(frontier_id_parts[4:])
+
+    except Exception:
+        print "\nLogException:",  line
+        return None
+    
+    return record
+
+if __name__ == '__main__':
+    sys.exit(main())
